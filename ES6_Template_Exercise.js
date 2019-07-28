4dc30195679932084b9b3acd7206e7b8dd661997
geraldg <geraldg@magenic.com>
2019-07-28 17:06:09 +0800

    add validations

--- ES6_Template_Exercise.js
+++ ES6_Template_Exercise.js
@@ -20,18 +20,32 @@ function wordCounter(result) {
 }
 
 
-function showTemplateResult(results, wordCount, vowelCount, consonantCount) {
-	console.info(`Input: ${results}
-		Number of word(s) found: ${wordCount}
-		Number of vowel(s) found: ${vowelCount}
-		Number of consonant(s) found: ${consonantCount}`);
+function showTemplateResult(dataString, wordCount, vowelCount, consonantCount) {
+	var result = `Input: ${dataString}`;
+	
+	result += `\nNumber of word(s) found: ${wordCount}`;
+	
+	if(wordCount > 0) {
+		result += `\nNumber of vowel(s) found: ${vowelCount}`;
+	}
+	
+	if(consonantCount > 0) {
+		result += `\nNumber of consonant(s) found: ${consonantCount}`;
+	}
+	
+	return console.info(result);
 }
 
 // dummy data
 
-var dummyData = "The quick brown QA jumps over the lazy Dev.";
+var dummyData = "The quick brown QA jumps over the lazy Dev. The quick brown QA jumps over the lazy Dev.";
 
 function wordProcessor(dataString) {
+	if(dataString.length < 50) {
+		return console.info(`ERROR: The minimum length of the input string should be at least 
+			50 characters for it to proceed`);
+	}
+	
 	var wordCount = wordCounter(dataString);
 	var vowelCount = vowelCounter(dataString);
 	var consonantCount = consonantCounter(dataString);
