02f78935b2818dc53bb358b7b6e4e360a1ea662f
geraldg <geraldg@magenic.com>
2019-07-29 14:11:02 +0800

    renaming and enhance the use of destructuring

--- ES6_Destructuring_Exercise.js
+++ ES6_Destructuring_Exercise.js
@@ -13,13 +13,13 @@ var dummyData = [
 ];
 
 function contestantOverallDetails(contestant, contestantsDetails) {
-	var result = { isTied: false, scoreEqualityCount: 0 };
+	var result = { isTied: false, tiedScoreCount: 0 };
 	
-	result.scoreEqualityCount = contestantsDetails.filter(function(contestantDetails) {
+	result.tiedScoreCount = contestantsDetails.filter(function(contestantDetails) {
 		return contestantDetails.score === contestant.score;
 	}).length;
 	
-	if(result.scoreEqualityCount > 1) {
+	if(result.tiedScoreCount > 1) {
 		result.isTied = true;
 	}
 	
@@ -38,7 +38,7 @@ function processRankingContestants(contestantsDetails) {
 		if(!finalizeContestantsDetails.some(function(details) { return details.score === contestant.score; })) {
 			if(contestantResultDetails.isTied) {
 				finalizeContestantsDetails.push({ 
-					name: contestantResultDetails.scoreEqualityCount.toString(), 
+					name: contestantResultDetails.tiedScoreCount.toString(), 
 					score: contestant.score 
 				});
 			} else {
@@ -52,25 +52,13 @@ function processRankingContestants(contestantsDetails) {
 
 function showTemplateResults(contestantsDetails) {
 	if(contestantsDetails.length > 3) {
-		let destructuringTopThreeContestants = [{name, score}, {name, score}, {name, score}] = processRankingContestants(contestantsDetails);
+		const [first, second, third] = processRankingContestants(contestantsDetails);
 		var result = `The winners are:\n`;
-		var topThree = 0;
 		
-		while(topThree < 3) {
-			
-			var contestant = destructuringTopThreeContestants[topThree];
-			
-			if(topThree === 0) {
-				result += `\n(1st) ${contestant.name} scored ${contestant.score} out of 10`;
-			} else if (topThree === 1) {
-				result += `\n(2nd) ${contestant.name} scored ${contestant.score} out of 10`;
-			} else {
-				result += `\n(3rd) ${contestant.name} scored ${contestant.score} out of 10`;
-			}
-			
-			topThree++;
-		}
-		
+		result += `\n(1st) ${first.name} scored ${first.score} out of 10`;
+		result += `\n(2nd) ${second.name} scored ${second.score} out of 10`;
+		result += `\n(3rd) ${third.name} scored ${third.score} out of 10`;
+	
 		return console.info(result);
 	} else {
 		return console.info(`Error: The minimum number of participant should be at least 3 to proceed.`);
